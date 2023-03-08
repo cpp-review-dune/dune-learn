80628ae40596c4f587b5f2d71599972ecaf9e04d
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-03-08 08:25:21 -0500

    Initial commit

---
+++ doc/cheat-sheet/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+#BUILD_DIR=$(pwd)/build
+
+./../../build/src/dune-learn > dune-learn-1.txt
---
+++ dune/learn/learn.hh
@@ -0,0 +1,21 @@
+#ifndef DUNE_LEARN_HH
+#define DUNE_LEARN_HH
+
+/** << NOTE
+ * \brief Defines a class for stuff.
+ * \tparam T Type to work with.
+ * \tparam NDim Number of dimensions.
+ */
+template <class T, int NDim>
+class myClass
+{
+
+private:
+  T num;
+
+public:
+  myClass();
+};
+// add your classes here
+
+#endif // DUNE_LEARN_HH

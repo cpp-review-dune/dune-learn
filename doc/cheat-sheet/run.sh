ed55938ede0860b20b9e49a4abf86ddb2f1b913b
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-03-20 19:42:52 -0500

    Update

--- doc/cheat-sheet/run.sh
+++ doc/cheat-sheet/run.sh
@@ -2,11 +2,12 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 BUILD_DIR="${REPO_ROOT}"/build
+CHEAT_SHEET_DIR="${REPO_ROOT}"/doc/cheat-sheet
 LOG_FILE=dune-learn-1.txt
 
 [ -d "${BUILD_DIR}" ] && "${BUILD_DIR}"/src/dune-learn >${LOG_FILE}
 
-jupyter nbconvert --to markdown ${REPO_ROOT}/src/hello.ipynb --stdout >${REPO_ROOT}/doc/cheat-sheet/hello.md
-pandoc -F ${REPO_ROOT}/doc/cheat-sheet/pandoc-minted.py -f markdown -t latex ${REPO_ROOT}/doc/cheat-sheet/hello.md -o ${REPO_ROOT}/doc/cheat-sheet/hello.tex
+jupyter nbconvert --to markdown ${REPO_ROOT}/src/hello.ipynb --stdout >${CHEAT_SHEET_DIR}/hello.md
+pandoc -F ${REPO_ROOT}/doc/cheat-sheet/pandoc-minted.py -f markdown -t latex ${CHEAT_SHEET_DIR}/hello.md -o ${CHEAT_SHEET_DIR}/hello.tex
 # --listings
 # -H "${REPO_ROOT}"/doc/cheat-sheet/header.md \
